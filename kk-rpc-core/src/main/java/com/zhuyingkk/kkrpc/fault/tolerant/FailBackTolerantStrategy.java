ac487d32f1697275209c6675fab60fbadd1ce48c
XJTUwyc <xjtuwyc@stu.xjtu.edu.cn>
2025-06-05 15:37:21 +0800

    fill fail-back and fail-over strategy

--- kk-rpc-core/src/main/java/com/zhuyingkk/kkrpc/fault/tolerant/FailBackTolerantStrategy.java
+++ kk-rpc-core/src/main/java/com/zhuyingkk/kkrpc/fault/tolerant/FailBackTolerantStrategy.java
@@ -1,8 +1,13 @@
 package com.zhuyingkk.kkrpc.fault.tolerant;
 
+import com.zhuyingkk.kkrpc.model.RpcRequest;
 import com.zhuyingkk.kkrpc.model.RpcResponse;
+import com.zhuyingkk.kkrpc.proxy.ServiceProxy;
+import com.zhuyingkk.kkrpc.proxy.ServiceProxyFactory;
 import lombok.extern.slf4j.Slf4j;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Map;
 
 /**
@@ -13,7 +18,40 @@ public class FailBackTolerantStrategy implements TolerantStrategy {
 
     @Override
     public RpcResponse doTolerant(Map<String, Object> context, Exception e) {
-        // todo 可自行扩展，获取降级的服务并调用
-        return null;
+        // 获取降级的服务并调用
+        RpcRequest rpcRequest = (RpcRequest) context.getOrDefault("rpcReqest", null);
+        if (rpcRequest == null) {
+            log.info("FailBackTolerantStrategy", e);
+            throw new RuntimeException(e.getMessage());
+        }
+        String serviceName = rpcRequest.getServiceName();
+        Class<?> serviceClass = null;
+        try {
+            serviceClass = Class.forName(serviceName);
+        } catch (ClassNotFoundException ex) {
+            throw new RuntimeException(ex);
+        }
+        // 使用模拟接口
+        Object mockProxy = ServiceProxyFactory.getMockProxy(serviceClass);
+        Method method = null;
+        try {
+            method = mockProxy.getClass().getMethod(rpcRequest.getMethodName(), rpcRequest.getParameterTypes());
+        } catch (NoSuchMethodException ex) {
+            throw new RuntimeException(ex);
+        }
+        Object result = null;
+        try {
+            result = method.invoke(mockProxy, rpcRequest.getArgs());
+        } catch (IllegalAccessException ex) {
+            throw new RuntimeException(ex);
+        } catch (InvocationTargetException ex) {
+            throw new RuntimeException(ex);
+        }
+        // 返回结果
+        RpcResponse rpcResponse = new RpcResponse();
+        rpcResponse.setData(result);
+        rpcResponse.setDataType(method.getReturnType());
+        rpcResponse.setMessage("Fail Back Tolerant strategy");
+        return rpcResponse;
     }
 }
--- kk-rpc-core/src/main/java/com/zhuyingkk/kkrpc/fault/tolerant/FailOverTolerantStrategy.java
+++ kk-rpc-core/src/main/java/com/zhuyingkk/kkrpc/fault/tolerant/FailOverTolerantStrategy.java
@@ -1,8 +1,21 @@
 package com.zhuyingkk.kkrpc.fault.tolerant;
 
+import cn.hutool.core.collection.CollUtil;
+import com.zhuyingkk.kkrpc.RpcApplication;
+import com.zhuyingkk.kkrpc.config.RpcConfig;
+import com.zhuyingkk.kkrpc.fault.retry.RetryStrategy;
+import com.zhuyingkk.kkrpc.fault.retry.RetryStrategyFactory;
+import com.zhuyingkk.kkrpc.loadbalancer.LoadBalancer;
+import com.zhuyingkk.kkrpc.loadbalancer.LoadBalancerFactory;
+import com.zhuyingkk.kkrpc.model.RpcRequest;
 import com.zhuyingkk.kkrpc.model.RpcResponse;
+import com.zhuyingkk.kkrpc.model.ServiceMetaInfo;
+import com.zhuyingkk.kkrpc.server.tcp.VertxTcpClient;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -13,7 +26,48 @@ public class FailOverTolerantStrategy implements TolerantStrategy {
 
     @Override
     public RpcResponse doTolerant(Map<String, Object> context, Exception e) {
-        // todo 可自行扩展，获取其他服务节点并调用
-        return null;
+        // 获取其他服务节点并调用
+        RpcRequest rpcRequest = (RpcRequest) context.get("rpcRequest");
+        List<ServiceMetaInfo> serviceMetaInfoList = (List<ServiceMetaInfo>) context.get("serviceMetaInfoList");
+        ServiceMetaInfo selectServiceMetaInfo = (ServiceMetaInfo) context.get("selectServiceMetaInfo");
+
+        // 移除失败节点
+        removeFailNode(selectServiceMetaInfo, serviceMetaInfoList);
+
+        RpcConfig rpcConfig = RpcApplication.getRpcConfig();
+        LoadBalancer loadBalancer = LoadBalancerFactory.getInstance(rpcConfig.getLoadBalancer());
+        Map<String, Object> requestParamMap = new HashMap<>();
+        requestParamMap.put("methodName", rpcRequest.getMethodName());
+
+        RpcResponse rpcResponse = null;
+        while (serviceMetaInfoList.size() > 0 || rpcResponse != null) {
+            ServiceMetaInfo currentServiceMetaInfo = loadBalancer.select(requestParamMap, serviceMetaInfoList);
+            System.out.println("获取节点：" + currentServiceMetaInfo);
+            try {
+                // 发送tcp请求
+                RetryStrategy retryStrategy = RetryStrategyFactory.getInstance(rpcConfig.getRetryStrategy());
+                rpcResponse = retryStrategy.doRetry(() -> VertxTcpClient.doRequest(rpcRequest, currentServiceMetaInfo));
+                return rpcResponse;
+            } catch (Exception exception) {
+                // 移除失败节点
+                removeFailNode(currentServiceMetaInfo, serviceMetaInfoList);
+                continue;
+            }
+        }
+        // 调用失败
+        throw new RuntimeException(e);
     }
+
+    private void removeFailNode(ServiceMetaInfo currentServiceMetaInfo, List<ServiceMetaInfo> serviceMetaInfoList) {
+        if (CollUtil.isNotEmpty(serviceMetaInfoList)) {
+            Iterator<ServiceMetaInfo> iterator = serviceMetaInfoList.iterator();
+            while (iterator.hasNext()) {
+                ServiceMetaInfo next = iterator.next();
+                if (currentServiceMetaInfo.getServiceNodeKey().equals(next.getServiceNodeKey())) {
+                    iterator.remove();
+                }
+            }
+        }
+    }
+
 }
